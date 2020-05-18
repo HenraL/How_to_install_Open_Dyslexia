019e62aa6825b51c2a97d3d3901884f0a96507a2
Hanra <37299738+HenraL@users.noreply.github.com>
2020-05-18 17:18:20 +0200

    Add files via upload

---
+++ files/open dislexia/js/script.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function() {
+
+	var $toggleButton = $('.toggle-button'),
+    	$menuWrap = $('.menu-wrap'),
+    	$sidebarArrow = $('.sidebar-menu-arrow');
+
+	// Hamburger button
+
+	$toggleButton.on('click', function() {
+		$(this).toggleClass('button-open');
+		$menuWrap.toggleClass('menu-show');
+	});
+
+	// Sidebar navigation arrows
+
+	$sidebarArrow.click(function() {
+		$(this).next().slideToggle(300);
+	});
+
+});
